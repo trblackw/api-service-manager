4432d9314eb51b08145ff326cae026d9eb4dc6b5
Tucker <trblackw@ncsu.edu>
2019-08-03 17:30:12 -0400

    Add basic JSON type/shape

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import { KeyValue } from './types';
+
+export const JSON: KeyValue<string, string>[] = [
+   { key: 'Accept', value: 'application/json' },
+   { key: 'Content-Type', value: 'application/json' }
+];
